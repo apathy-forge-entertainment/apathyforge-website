67540bc1f83de0905251d2c016b6ecdc81167a36
Branden Barber <37583030+froreWolf@users.noreply.github.com>
2020-02-06 12:16:33 -0500

    Chaos Garden: created framework

---
+++ Chaos_Garden/Insert_Functions.js
@@ -0,0 +1,8 @@
+function getNavBar()
+{
+    return  "<ul>" +
+                "<li><a href='index.html'>Home</a><li>" +
+                "<li><a href='play.html'>Play</a><li>" +
+                "<li><a href='learn.html'>Learn</a></li>" +
+            "</ul>"
+}
