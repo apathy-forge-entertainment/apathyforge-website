1234f101a5d6fa5bd05c3bc46b6a2e94fd1f34ff
Branden Barber <37583030+froreWolf@users.noreply.github.com>
2020-05-28 13:57:13 -0400

    restructing site

---
+++ resources/Javascript/common.js
@@ -0,0 +1,18 @@
+function getHeader()
+{
+    var  navBar = document.getElementById("header");
+    navBar.innerHTML = 
+    '<ul id="navBar">' +
+    '<li><a href="/index.html">Home</a></li>' + 
+    '<li><a href="/news.html">News</a></li>' +
+    '<li><a href="/projects.html">Projects</a></li>' + 
+    '<li><a href="/about.html">About</a></li>' +
+    '<li><a href="/contact.html">Contact Us</a></li>' + 
+    '</ul>';
+}
+
+function getFooter()
+{
+    var footer = document.getElementById("footer");
+    footer.innerHTML = '<p>Apathy Forge Entertainment | Site Updated: May 2020</p>';
+}
